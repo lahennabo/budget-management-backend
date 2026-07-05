d3d90fdde0e5d39462fee036e30417b87d18f064
lah.ennabo <lah.ennabo@gmail.com>
2026-07-05 20:52:35 +0200

    indiquer le package des entites

--- src/main/java/com/budget/management/BudgetManagementApplication.java
+++ src/main/java/com/budget/management/BudgetManagementApplication.java
@@ -2,8 +2,10 @@ package com.budget.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.budget.management.entity")
 public class BudgetManagementApplication {
 
     public static void main(String[] args) {
